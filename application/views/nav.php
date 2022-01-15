28b7b8dcb3ffb4aaa403c6db25337e15437ad8b3
cpandeysoftgame <86830961+cpandeysoftgame@users.noreply.github.com>
2022-01-15 11:42:27 +0530

    navbar fix for mobile

--- application/views/nav.php
+++ application/views/nav.php
@@ -1,5 +1,5 @@
-  <!-- ======= Header ======= -->
-  <header id="header" class="fixed-top d-flex align-items-center">
+ <!-- ======= Header ======= -->
+ <header id="header" class="fixed-top d-flex align-items-center">
     <div class="container d-flex align-items-center justify-content-between">
 
       <div class="logo">
@@ -10,14 +10,19 @@
 
       <nav id="navbar" class="navbar">
         <ul>
+          
           <li><a class="nav-link scrollto" href="<?php echo base_url(); ?>">Home</a></li>
           <li><a class="nav-link scrollto" href="<?php echo base_url('About-Us'); ?>"/>About Us</a></li>
           <li><a class="nav-link scrollto" href="<?php echo base_url('Services'); ?>">Services</a></li>
           <li><a class="nav-link scrollto" href="<?php echo base_url('Products'); ?>">Products</a></li>
           <li><a class="nav-link scrollto" href="<?php echo base_url('Contacts'); ?>">Contact</a></li>
           <li><a class="getstarted scrollto" href="<?php echo base_url('Book-Your-Appointment'); ?>">Book Your Appointment</a></li>
+
+
+        </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
       </nav><!-- .navbar -->
 
     </div>
   </header><!-- End Header -->
+ 
